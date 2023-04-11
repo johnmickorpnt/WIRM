ba12314532c90804113e762abb88453558c2f179
johnmickorpnt <69314516+johnmickorpnt@users.noreply.github.com>
2023-04-11 18:28:24 +0800

    Basic services update

--- services.php
+++ services.php
@@ -1,12 +1,49 @@
 <?php
 $title = "Services - Cafe Lupe";
-$containerStyles = "padding: 0 10rem 0 10rem; margin-top:15rem";
+$containerStyles = "padding: 0 10rem 0 10rem; margin-top:10rem";
 $content = <<<CONTENT
     <div class="hr-container">
         <hr>
         <h1>Our Menu</h1>
     </div>
-
+    <div class="gallery-container">
+        <div class="gallery-slide">
+            <img src="assets/imgs/menu/beverages1.webp" class="img-fluid">
+            <img src="assets/imgs/menu/beverages2.webp" class="img-fluid">
+            <img src="assets/imgs/menu/all-day-breakfast.webp" class="img-fluid">
+            <img src="assets/imgs/menu/cover.webp" class="img-fluid">
+            <img src="assets/imgs/menu/party-celeb.webp" class="img-fluid">
+            <img src="assets/imgs/menu/1.webp" class="img-fluid">
+            <img src="assets/imgs/menu/mexican.webp" class="img-fluid">
+        </div>
+        <div class="arrow-btn left-arrow">
+            <i class="fa-solid fa-chevron-left"></i>
+        </div>
+        <div class="arrow-btn right-arrow">
+            <i class="fa-solid fa-chevron-right"></i>
+        </div>
+    </div>
+    <div class="hr-container">
+        <hr>
+        <h1>CASA ALEGRIA HOSTEL ROOMS</h1>
+    </div>
+    <div class="gallery-container">
+        <div class="gallery-slide">
+            <img src="assets/imgs/menu/beverages1.webp" class="img-fluid">
+            <img src="assets/imgs/menu/beverages2.webp" class="img-fluid">
+            <img src="assets/imgs/menu/all-day-breakfast.webp" class="img-fluid">
+            <img src="assets/imgs/menu/cover.webp" class="img-fluid">
+            <img src="assets/imgs/menu/party-celeb.webp" class="img-fluid">
+            <img src="assets/imgs/menu/1.webp" class="img-fluid">
+            <img src="assets/imgs/menu/mexican.webp" class="img-fluid">
+        </div>
+        <div class="arrow-btn left-arrow">
+            <i class="fa-solid fa-chevron-left"></i>
+        </div>
+        <div class="arrow-btn right-arrow">
+            <i class="fa-solid fa-chevron-right"></i>
+        </div>
+    </div>
 CONTENT;
 ?>
 
